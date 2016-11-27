ed80bc1fde934ba21693a00358428da1fd503c46
Samhan Salahuddin <samhan@ABC-XYZ.local>
2016-11-27 13:15:19 +0530

    Add compression check

---
+++ compress.py
@@ -0,0 +1,30 @@
+def compress(uncompressed):
+    """Compress a string to a list of output symbols."""
+ 
+    # Build the dictionary.
+    dict_size = 256
+    dictionary = dict((chr(i), i) for i in xrange(dict_size))
+    # in Python 3: dictionary = {chr(i): i for i in range(dict_size)}
+ 
+    w = ""
+    result = []
+    for c in uncompressed:
+        wc = w + c
+        if wc in dictionary:
+            w = wc
+        else:
+            result.append(dictionary[w])
+            # Add wc to the dictionary.
+            dictionary[wc] = dict_size
+            dict_size += 1
+            w = c
+ 
+    # Output the code for w.
+    if w:
+        result.append(dictionary[w])
+    return result
+
+
+
+
+print(compress("CDECDE"))
